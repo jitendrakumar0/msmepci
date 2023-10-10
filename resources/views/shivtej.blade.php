1005971877b105636bf3b62edfd50e1465155776
shivtej <shivtej.img@gmail.com>
2023-10-10 15:12:56 +0530

    hero section

--- resources/views/shivtej.blade.php
+++ resources/views/shivtej.blade.php
@@ -12,10 +12,54 @@
 
 
 @section('content')
-index page
+
+
+<div class="heroSection container-fluid px-0">
+    <div class="row">
+      <div class="col-12">
+        <div class="swiper mySwiper heroSwiper">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide swiperSlider">
+             <div class="sliderImg"><img src="assets/img/herosection/01-2.png" alt="herosection" class="img-fluid w-100  h-100"> </div>
+            </div>
+            <div class="swiper-slide swiperSlider">
+              <div class="sliderImg"><img src="assets/img/herosection/02-1.png" alt="herosection" class="img-fluid w-100  h-100"> </div>
+             </div>
+             <div class="swiper-slide swiperSlider">
+              <div class="sliderImg"><img src="assets/img/herosection/03-1.png" alt="herosection" class="img-fluid w-100  h-100"> </div>
+             </div>
+             <div class="swiper-slide swiperSlider">
+              <div class="sliderImg"><img src="assets/img/herosection/04-1.png" alt="herosection" class="img-fluid w-100  h-100"> </div>
+             </div>
+          </div>
+          <div class="swiper-button-next"> <span class="d-inline-flex navIcon"><img src="assets/img/herosection/leftarrownav.png" alt=""></span> </div>
+          <div class="swiper-button-prev"><span class="d-inline-flex navIcon"><img src="assets/img/herosection/rightarrownav.png" alt=""></span> </div>
+          <div class="swiper-pagination"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
 @endsection
 
 
 @push('scripts')
+
+
+<script>
+    var swiper = new Swiper(".heroSwiper", {
+      spaceBetween: 30,
+      effect: "fade",
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  </script>
 
 @endpush
