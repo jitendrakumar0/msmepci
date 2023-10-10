38cc4f04fff8cff114570ca86ee34cb2499a565f
pallavi.img <122247897+pallaviimg@users.noreply.github.com>
2023-10-10 15:30:04 +0530

    swiper

--- resources/views/pallavi.blade.php
+++ resources/views/pallavi.blade.php
@@ -11,13 +11,55 @@
 
 
 @section('content')
-<div class="container-fluid linksFluid">
+<div class="container-fluid linksFluid py-5">
     <div class="container">
-        <div class="row">
-            <div class="col-4">
-                <div class="row linkWrapper">
+        <div class="row g-3 mx-0">
+            <div class="col-3">
+                <div class="row linkWrapper mx-0">
                     <div class="col-12"><div class="linksIcon"><img src="{{ url('public/assets/img/copy.svg')}}" alt=""></div></div>
-                    <div class="col-12"><a href="javascript:;" class="linksText">MSME SAMADHAN</a></div>
+                    <div class="col-12"><a href="javascript:;" class="linksText d-flex align-items-center">MSME SAMADHAN <img src="{{ url('public/assets/img/rytArrow.svg')}}" alt=""></a></div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="row linkWrapper mx-0">
+                    <div class="col-12"><div class="linksIcon"><img src="{{ url('public/assets/img/copy.svg')}}" alt=""></div></div>
+                    <div class="col-12"><a href="javascript:;" class="linksText d-flex align-items-center">MSME SAMADHAN <img src="{{ url('public/assets/img/rytArrow.svg')}}" alt=""></a></div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="row linkWrapper mx-0">
+                    <div class="col-12"><div class="linksIcon"><img src="{{ url('public/assets/img/copy.svg')}}" alt=""></div></div>
+                    <div class="col-12"><a href="javascript:;" class="linksText d-flex align-items-center">MSME DATABASE <img src="{{ url('public/assets/img/rytArrow.svg')}}" alt=""></a></div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="row linkWrapper mx-0">
+                    <div class="col-12"><div class="linksIcon"><img src="{{ url('public/assets/img/copy.svg')}}" alt=""></div></div>
+                    <div class="col-12"><a href="javascript:;" class="linksText d-flex align-items-center">E PARTICIPATION <img src="{{ url('public/assets/img/rytArrow.svg')}}" alt=""></a></div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="row linkWrapper mx-0">
+                    <div class="col-12"><div class="linksIcon"><img src="{{ url('public/assets/img/copy.svg')}}" alt=""></div></div>
+                    <div class="col-12"><a href="javascript:;" class="linksText d-flex align-items-center">MSME SAMPARK <img src="{{ url('public/assets/img/rytArrow.svg')}}" alt=""></a></div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="row linkWrapper mx-0">
+                    <div class="col-12"><div class="linksIcon"><img src="{{ url('public/assets/img/copy.svg')}}" alt=""></div></div>
+                    <div class="col-12"><a href="javascript:;" class="linksText d-flex align-items-center">UDHYAM AADHAR <img src="{{ url('public/assets/img/rytArrow.svg')}}" alt=""></a></div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="row linkWrapper mx-0">
+                    <div class="col-12"><div class="linksIcon"><img src="{{ url('public/assets/img/copy.svg')}}" alt=""></div></div>
+                    <div class="col-12"><a href="javascript:;" class="linksText d-flex align-items-center">PMEG E PORTAL <img src="{{ url('public/assets/img/rytArrow.svg')}}" alt=""></a></div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="row linkWrapper mx-0">
+                    <div class="col-12"><div class="linksIcon"><img src="{{ url('public/assets/img/copy.svg')}}" alt=""></div></div>
+                    <div class="col-12"><a href="javascript:;" class="linksText d-flex align-items-center">CHAMPION SCHEME <img src="{{ url('public/assets/img/rytArrow.svg')}}" alt=""></a></div>
                 </div>
             </div>
         </div>
