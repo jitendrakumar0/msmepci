2ce873c7dfe6e0fd9abd9ace9c0b816324b142bc
Jitendra Kumar Bijarnia <70063345+jitendrakumar0@users.noreply.github.com>
2023-10-10 12:45:20 +0530

    init

---
+++ resources/views/main.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @stack('meta')
+
+        <title>MSMEPCI</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="{{asset('/public')}}/assets/css/bootstrap.min.css" />
+        @stack('styles')
+    </head>
+    <body>
+        @include('layouts.header')
+        <main>
+            @yield('content')
+        </main>
+        @include('layouts.footer')
+        @stack('scripts')
+        <script src="{{asset('/public')}}/assets/libs/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
