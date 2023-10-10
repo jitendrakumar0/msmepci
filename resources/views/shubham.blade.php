c516de88b8fd4e533ad76d6fb0c3df3aa19c91cb
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2023-10-10 16:31:24 +0530

    uncleji Section

--- resources/views/shubham.blade.php
+++ resources/views/shubham.blade.php
@@ -12,7 +12,22 @@
 
 
 @section('content')
-index page
+    <div class="container-fluid chairmanSection">
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <div class="chairmanImg">
+                        <img src="{{ url('public/assets/img/Chairman.webp')}}" alt="chairmanImg" class="h-100 w-100 img-fluid">
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="ChairmanName">
+                        Dr E Muthuraman
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 
