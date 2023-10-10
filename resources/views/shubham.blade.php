97b3b412fb9b4255e1bd9926e420a4c3cc6e7148
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2023-10-10 18:22:19 +0530

    chaiMan Section

--- resources/views/shubham.blade.php
+++ resources/views/shubham.blade.php
@@ -12,22 +12,74 @@
 
 
 @section('content')
-    <div class="container-fluid chairmanSection">
+    <div class="container-fluid chairmanSection bg-light py-4 py-md-5">
         <div class="container">
             <div class="row">
-                <div class="col-6">
-                    <div class="chairmanImg">
-                        <img src="{{ url('public/assets/img/Chairman.webp')}}" alt="chairmanImg" class="h-100 w-100 img-fluid">
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="ChairmanName">
-                        Dr E Muthuraman
+                <div class="col-12">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-sm-10 col-lg-8 col-xl-6">
+                            <div class="row ">
+                                <div class="col-12">
+                                    <div class="chairmanImg overflow-hidden rounded-3">
+                                        <img src="{{ url('public/assets/img/chairmanSecond.jpg')}}" alt="chairmanImg" class="h-100 w-100 img-fluid object-fit-cover">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="ChairmanName text-center">
+                                        Dr E Muthuraman
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="position text-center">
+                                        Chairman
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="description text-center pt-3">
+                                        MSME Promotion Council Chairman Shri Dr. E. Muthuraman has welcomed the much-awaited Foreign Trade Policy from the Commerce Ministry under the guidance of Hon’ble Prime Minister Shri Narendra Modi Ji and Commerce and Industry Minister Shri Piyush Goyal Ji, which broke the traditional Five-Year policy lasting term and adopted the ‘Long Term’ focus for the policy, giving a visionary initiative to our country.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <div class="container-fluid chairmanTwoSection bg-light py-4 py-md-5">
+        <div class="container">
+            <div class="row gap-2 justify-content-center align-items-center">
+                <div class="col-12 col-sm-10 col-lg-5">
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <div class="chairmanImg overflow-hidden rounded-3">
+                                <img src="{{ url('public/assets/img/chairmanSecond.jpg')}}" alt="chairmanImg" class="h-100 w-100 img-fluid object-fit-cover">
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="col-12 col-sm-10 col-lg-6">
+                    <div class="col-12">
+                        <div class="ChairmanName text-center text-lg-start">
+                            Dr E Muthuraman
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="position text-center text-lg-start pt-2 pt-xl-3">
+                            Chairman
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="description text-center text-lg-start pt-2 pt-xl-3">
+                            MSME Promotion Council Chairman Shri Dr. E. Muthuraman has welcomed the much-awaited Foreign Trade Policy from the Commerce Ministry under the guidance of Hon’ble Prime Minister Shri Narendra Modi Ji and Commerce and Industry Minister Shri Piyush Goyal Ji, which broke the traditional Five-Year policy lasting term and adopted the ‘Long Term’ focus for the policy, giving a visionary initiative to our country.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
 
 
