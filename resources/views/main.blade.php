7de39b817f2b1d0b75c0875a69aebe38fa343d4b
shubham agrawal <145680136+shubhamimg@users.noreply.github.com>
2023-10-11 12:23:38 +0530

    glightSection

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,11 +8,15 @@
         <title>MSMEPCI</title>
 
         <!-- Fonts -->
+        
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        
+        <script src="{{asset('/public')}}/assets/libs/jquery/jquery.js"></script>
         <link rel="stylesheet" href="{{asset('/public')}}/assets/css/bootstrap.min.css" />
         <link rel="stylesheet" href="{{asset('/public')}}/assets/css/swiper.min.css" />
         <link rel="stylesheet" href="{{asset('/public')}}/assets/css/style.min.css" />
+        <link rel="stylesheet" href="{{asset('/public')}}/assets/libs/glightBox/glightbox.min.css"/>
         @stack('styles')
     </head>
     <body>
@@ -24,6 +28,7 @@
         @stack('scripts')
         <script src="{{asset('/public')}}/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
         <script src="{{asset('/public')}}/assets/libs/swiper/swiper-bundle.min.js"></script>
+        <script src="{{asset('/public')}}/assets/libs/glightBox/glightbox.min.js"></script>
         @stack('scripts')
     </body>
 </html>
